92dae2b1337bb1622f3db22d1b7c9d4ce377452d
Kvn22xx <kevinnelson9296@gmail.com>
2021-10-27 00:45:02 +0530

    gallery final

--- script.js
+++ script.js
@@ -12,23 +12,77 @@
 //   setTimeout(showSlides, 3000); 
 // }
 
+var modal = document.getElementById('myModal');
+var img = $(".myImg");
+var modalImg = $("#img01");
+var captionText = document.getElementById("caption");
+$('.myImg').click(function(){
+    modal.style.display = "block";
+    var newSrc = this.src;
+    modalImg.attr('src', newSrc);
+    captionText.innerHTML = this.alt;
+});
+var span = document.getElementsByClassName("close")[0];
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+//JS code for gallery slideshow
+var gal_slideIndex=1;
+var gal_myTimer;
+var gal_container;
+
+window.addEventListener("load",function(){
+  galshowSlides(gal_slideIndex);
+  gal_myTimer = setInterval(function(){galplusSlides(1)}, 4000);
+
+  gal_container = document.getElementsByClassName('galslide-inner')[0];
+  gal_container.addEventListener('mouseenter',pause)
+  gal_container.addEventListener('mouseleave',resume)
+})
+
+function galplusSlides(n){
+  clearInterval(gal_myTimer);
+  if(n<0)
+  {
+    galshowSlides(gal_slideIndex -=1);
+  }
+  else{
+    galshowSlides(gal_slideIndex +=1);
+  }
+
+  if(n===-1)
+  {
+    gal_myTimer = setInterval(function(){galplusSlides(n + 2)}, 4000);
+  }
+  else{
+    gal_myTimer = setInterval(function(){galplusSlides(n+1)}, 4000);
+  }
+}
+function galcurrentSlide(n){
+  clearInterval(gal_myTimer);
+  gal_myTimer = setInterval(function(){galplusSlides(n + 1)}, 4000);
+  galshowSlides(gal_slideIndex = n);
+}
+
+function galshowSlides(n){
+  var i;
+  var slides = document.getElementsByClassName("gal");
+  var galdots = document.getElementsByClassName("gal-dot");
+  if (n > slides.length) {gal_slideIndex = 1}
+  if (n < 1) {gal_slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < galdots.length; i++) {
+      galdots[i].className = galdots[i].className.replace(" active", "");
+  }
+  slides[gal_slideIndex-1].style.display = "block";
+  galdots[gal_slideIndex-1].className += " active";
+}
 
 
-// var modal = document.getElementById('myModal');
-// var img = $(".myImg");
-// var modalImg = $("#img01");
-// var captionText = document.getElementById("caption");
-// $('.myImg').click(function(){
-//     modal.style.display = "block";
-//     var newSrc = this.src;
-//     modalImg.attr('src', newSrc);
-//     captionText.innerHTML = this.alt;
-// });
-// var span = document.getElementsByClassName("close")[0];
-// span.onclick = function() {
-//   modal.style.display = "none";
-// }
-
+//JS code for latest from season slideshow
 var slideIndex = 1;
 
 var myTimer;
